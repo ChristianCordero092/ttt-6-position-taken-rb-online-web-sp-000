7d6348ebde91fd44864ec8643a8eb2cddd9468f2
Christian Cordero <christiancordero092@gmail.com>
2021-05-06 19:22:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,13 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  if board[0] == 
+  board[0] == "X" || "O"
+  board[1] == "X" || "O"
+  board[2]  == "X" || "O"
+  board[3] == "X" || "O"
+  board[4] == "X" || "O"
+  board[5] == "X"  || "O"
+  board[6] == "X" || "O"
+  board[7] == "X" || "O"
+  board[8] == "X" || "O"
 end
